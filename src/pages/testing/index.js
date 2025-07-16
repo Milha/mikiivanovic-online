638c6775bc74a3819d6dd01c128a56cadbedda5e
Miki <mickeytuts2401@gmail.com>
2025-07-16 20:39:34 +0200

    add nav bar, index page and testing page

--- src/pages/testing/index.js
+++ src/pages/testing/index.js
@@ -1,5 +1,13 @@
-import classes from "./index.module.css";
+import Image from "next/image";
+import classes from "./index.module.scss";
 
 export default function TestingPage() {
-  return <div className={classes.cont}></div>;
+  return (
+    <div className={classes.cont}>
+      {/* <p>Prikaz slike</p> */}
+      <div className={classes.image_cont}>
+        <Image src="/images/flowers/cvece-01.jpg" fill />
+      </div>
+    </div>
+  );
 }
